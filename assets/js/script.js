c2894992d766028b14ddbc09f41982474aad8288
ashachakre0906 <chourpagar.asha@gmail.com>
2022-05-18 20:36:46 -0700

    changed the background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ var startButton = document.getElementById("start-quiz");
 startButton.addEventListener("click",renderQuestion);
 var headerEl = document.getElementById("header")
 var mainEl = document.getElementById("main")
+// var questionArr=[""];
 function renderQuestion(){
-    headerEl.classList.add("hidden");//
-    mainEl.classList.remove("hidden");
-
+    headerEl.classList.add("hidden");//Targets the header element which will add the class hidden
+    mainEl.classList.remove("hidden");//Targets the main element which will remove the class hidden
 }
