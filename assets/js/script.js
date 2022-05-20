b678118308c2aa575c5568a9e05cda3e8d4269c0
ashachakre0906 <chourpagar.asha@gmail.com>
2022-05-19 17:19:11 -0700

    creatd variables and added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var startButton = document.getElementById("start-quiz");
 startButton.addEventListener("click",renderQuestion);
+var createHighScore = document.getElementById("create-high-score");
+createHighScore.addEventListener("click",highScore);
+var gobackbutton = document.getElementById("go-Back");
+gobackbutton.addEventListener("click",renderStartPage);
+var clearScores =document.getElementById("clear-high-score");
+clearScores.addEventListener("click",clearScores);
 var headerEl = document.getElementById("header")
 var mainEl = document.getElementById("main")
 // var questionArr=[""];
@@ -7,3 +13,6 @@ function renderQuestion(){
     headerEl.classList.add("hidden");//Targets the header element which will add the class hidden
     mainEl.classList.remove("hidden");//Targets the main element which will remove the class hidden
 }
+
+
+
